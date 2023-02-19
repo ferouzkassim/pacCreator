9f4f48c8719c48ca2f4c87f8dfaab6c0577e41bb
ferouz kassim <ferouzkassim@gmail.com>
2023-02-19 22:33:49 +0300

    adding gui seprated gi frmo paccreTOR

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import tkinter as tk
+import xml.etree.ElementTree as Et
 root = tk.Tk()
 from cfgfilecreator import *
-
+xmlfile = ('sample.tdf')
 root.config(height=400,width=600)
 root.maxsize(height=600,width=800)
 frame1 = tk.Frame(root,height=300,width=500)
-cpu_options= []
+cpu_options= ('T606','T606_64','s7731E')
 cpu_iterator = tk.StringVar()
 cpu_iterator.set('cpu_model')
 optionmenu = tk.OptionMenu(frame1,cpu_iterator,*cpu_options)
 optionmenu.grid(column=0,row=0)
 frame1.pack()
-root.mainloop()
+xmlpars = Et.parse(xmlfile)
+xmlroot = xmlpars.getroot()
+for sector in xmlroot:
+    print(sector.tag)
+
+#root.mainloop()
